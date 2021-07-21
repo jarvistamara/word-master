505e94e284f6bc3d8321f4aeee27ef4fa38e178f
jarvistamara <tamara.jarvis17@icloud.com>
2021-07-21 14:59:42 -0400

    testing

--- WordMasterApp/src/components/app.js
+++ WordMasterApp/src/components/app.js
@@ -1,20 +1,20 @@
 import React, { Component } from 'react';
+<<<<<<< HEAD
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Home from './Home';
 import { Dashboard } from './Dashboard';
 
 class App extends Component {
+=======
+
+export default class App extends Component {
+>>>>>>> parent of e7805809 (react file setup)
   render() {
     return (
       <div className='app'>
-        <BrowserRouter>
-        <Switch>
-        <Route exact path={'/'} component={Home} />
-        <Route exact path={'/dashboard'} component={Dashboard} />
-        </Switch>
-        </BrowserRouter>
+        <h1>DevCamp React Starter</h1>
+        <h2>React Redux Router</h2>
       </div>
     );
   }
 }
-export default App;

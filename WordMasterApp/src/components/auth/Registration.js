4933b7dc5fb977c33cdc46c740cf68ca4610e711
jarvistamara <tamara.jarvis17@icloud.com>
2021-07-21 16:38:27 -0400

    working on auth Registration

---
+++ WordMasterApp/src/components/auth/Registration.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class Registration extends Component {
+    render() {
+        return (
+            <div>
+                Registration goes here
+            </div>
+        )
+    }
+}
+export default Registration

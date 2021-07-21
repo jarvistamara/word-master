2c6da0f01ceb582a1814118b95e9bf4650f5d592
jarvistamara <tamara.jarvis17@icloud.com>
2021-07-21 13:30:35 -0400

    fixed registrations controller

--- WordMasterApp/src/components/app.js
+++ WordMasterApp/src/components/app.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Home from './Home';
 import { Dashboard } from './Dashboard';
+
 class App extends Component {
   render() {
     return (

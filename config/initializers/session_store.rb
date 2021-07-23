a8c707f7679e679b5a5a2f8c7f7cc2c3d230e7c1
jarvistamara <tamara.jarvis17@icloud.com>
2021-07-23 09:32:42 -0400

    Update session_store.rb

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,5 @@
-Rails.application.config.session_store :cookie_store, key: "_word_master_authentication", domain: "nil", secure: true, httponly: false, same_site::none
+if Rails.env == "production"
+    Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "jdh-authentication-app-api.herokuapp.com"
+  else
+    Rails.application.config.session_store :cookie_store, key: "_authentication_app"
+  end
